9a58d1bb0d582c3fff516dfe74d8c5504ff06f36
unblinky <>
2025-08-20 19:58:28 -0700

    Level manager started.

--- Code/Level/Level.gd
+++ Code/Level/Level.gd
@@ -1,2 +1,22 @@
 extends Node3D
 class_name Level
+
+const PLAYER = preload("res://Player/Player.tscn")
+
+@onready var spawn_point = $SpawnPoint
+@onready var exit = $Exit
+
+var main: Main
+
+func _ready() -> void:
+	exit.body_entered.connect(OnExit)
+	
+	var player: Player
+	player = PLAYER.instantiate()
+	player.position = spawn_point.position
+	add_child(player)
+
+func OnExit(body):
+	print("Exiting")
+	if body is Player:
+		main.NextLevel()
--- Code/Main/Main.gd
+++ Code/Main/Main.gd
@@ -1,6 +1,26 @@
 extends Node
 class_name Main
 
+var levels: Array[PackedScene] = [preload("res://Levels/Level_01.tscn"), preload("res://Levels/Level_02.tscn")]
+var level_index: int = -1
+var current_level: Level
+
+func _ready() -> void:
+	NextLevel()
+
+func NextLevel():
+	# Increment level index, or wrap to first level.
+	if level_index < levels.size() - 1:
+		level_index += 1
+	else:
+		level_index = 0
+	
+	if current_level != null:
+		current_level.queue_free()
+	current_level = levels[level_index].instantiate()
+	current_level.main = self
+	add_child(current_level)
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
